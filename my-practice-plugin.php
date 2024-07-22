93d2efdc4a1c4ed178ff9674c04d6831d32ec1ab
Tuba <tubasaif718@gmail.com>
2024-07-22 17:20:59 +0500

    upload plugin

---
+++ my-practice-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: My Awesome Plugin
+ * Description: A plugin to demonstrate how to use Composer, npm, Node.js, ES6, Webpack, and Babel with WordPress.
+ * Version: 1.0
+ * Author: Your Name
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+// Enqueue the JavaScript file
+function my_awesome_plugin_enqueue_scripts() {
+    wp_enqueue_script('my-awesome-plugin-script', plugins_url('./dist/bundle.js', __FILE__), array(), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'my_awesome_plugin_enqueue_scripts');
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// src/index.js- Add some basic ES6 code to src/index.js
+const message = 'Hello from My Awesome Plugin!';
+console.log(message);
